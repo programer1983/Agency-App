d81576d7b8bfb8f8ca5da0345c8eb9b4f41ccaf1
programer1983 <programer972@gmail.com>
2021-11-19 20:03:34 +0200

    creating an intro;

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,10 +1,31 @@
 import styles from "./../styles/intro.module.css"
+import Image from "next/image"
 
 const Intro = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.card}>first</div>
-            <div className={styles.card}>second</div> 
+            <div className={styles.card}>
+                <h1 className={styles.title}>
+                    <span className={styles.brand}>AV0CADO </span>
+                    DIGITAL PRODUCT AGENCY
+                </h1>
+                <p className={styles.desc}>
+                    Create live segments and 
+                    target thr right people 
+                    for messages based on their behaviors.
+                </p>
+                <button className={styles.button}>DISCOVER</button>
+            </div>
+            <div className={styles.card}>
+                <Image 
+                   src="/img/Avocado.png" 
+                   width="100%" 
+                   height="100%"
+                   objectFit="cover"
+                   layout="fill"
+                   alt="" 
+                />
+            </div> 
         </div>
     )
 }
