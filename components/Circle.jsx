2fc371bf6e002d6035a1ead2ff86e72434070907
programer1983 <programer972@gmail.com>
2021-11-20 16:53:12 +0200

    creating circles with styles;

---
+++ components/Circle.jsx
@@ -0,0 +1,9 @@
+
+
+const Circle = (props) => {
+    return (
+        <div  className="circle" style={{...props}} />
+    )
+}
+
+export default Circle
--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,9 +1,12 @@
 import styles from "./../styles/intro.module.css"
 import Image from "next/image"
+import Circle from "./Circle"
 
 const Intro = () => {
     return (
         <div className={styles.container}>
+            <Circle backgroundColor="#b0ff49" top="-50vh" left="-50vh"/>
+            <Circle backgroundColor="#01c686" right="-40vh"/>
             <div className={styles.card}>
                 <h1 className={styles.title}>
                     <span className={styles.brand}>AV0CADO </span>
