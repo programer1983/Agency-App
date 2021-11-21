ed43b5ffe7fe107165c494f2dbaf21c89d11786f
programer1983 <programer972@gmail.com>
2021-11-21 11:54:14 +0200

    database creation, database connection, service component creation and data output;

---
+++ data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+        id: 1,
+        name: "design",
+        title: "Design",
+        desc: "Simply drag and drop photos and videos intro your workspace to auto",
+        video: "Avocado.mp4",
+        photo: "asdasd.png"
+    },
+    {
+        id: 2,
+        name: "development",
+        title: "Development",
+        desc: "Simply drag and drop photos and videos intro your workspace to auto",
+        photo: "dev.jpeg"
+    },
+    {
+        id: 3,
+        name: "production",
+        title: "Production",
+        desc: "Simply drag and drop photos and videos intro your workspace to auto",
+        photo: "production.jpeg"
+    },
+    {
+        id: 4,
+        name: "photography",
+        title: "Photography",
+        desc: "Simply drag and drop photos and videos intro your workspace to auto",
+        photo: "photography.jpeg"
+    }
+]
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Intro from '../components/Intro'
+import Services from '../components/Services'
+import { data } from '../data'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({services}) {
+ 
   return (
     <div>
       <Head>
@@ -10,6 +13,14 @@ export default function Home() {
         <meta name="description" content="The best YT Channel!" />
       </Head>
       <Intro />
+      <Services services={services} />
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const services = data
+  return {
+    props: { services },
+  }
 }
