4c69f5b5d7d2efb1f76483119132090ee97221e5
programer1983 <programer972@gmail.com>
2021-11-30 21:24:04 +0200

    hamburger content creation, animation;

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 import styles from "./../styles/Navbar.module.css"
 import Link from "next/link"
+import {useState} from "react"
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
     return (
         <div className={styles.container}>
             <Link href="/">AVOCADO</Link>
@@ -22,11 +25,28 @@ const Navbar = () => {
                     <Link href="/contact">CONTACT</Link>
                 </li>
             </ul>
-            <div className={styles.hanburger}>
+            <div className={styles.hanburger} onClick={() => setOpen(!open)}>
                 <div className={styles.line} />
                 <div className={styles.line} />
                 <div className={styles.line} />
             </div>
+            <ul onClick={() => setOpen(false)} className={styles.menu} style={{right: open ? "0px" : "-50vw"}}>
+                <li className={styles.menuItem}>
+                    <Link href="/products/design">DESIGN</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/products/development">DEVELOPMENT</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/products/production">PRODUCTION</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/products/photography">PHOTOGRAPHY</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/contact">CONTACT</Link>
+                </li>
+            </ul>
         </div>
     )
 }
